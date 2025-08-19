e0211e7970be604b6884ef0a8d25f442a6b59c93
Mohammed Sinan <sinan@mohammeds-MacBook-Air.local>
2025-08-20 00:20:02 +0530

    personal portfolio initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const navLinkEls = document.querySelectorAll('.nav-link');
+
+navLinkEls.forEach(navLinkEl =>{
+    navLinkEl.addEventListener('click',()=>{
+        document.querySelector('.active')?.classList.remove('active');
+        navLinkEl.classList.add('active');
+
+    })
+})
